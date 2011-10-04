26d0fe87b06df827ad32831d802f5ae84f43d6ee
Tommy Bishop <bishop.thomas@gmail.com>
2011-10-04 15:59:59 -0700

    documentation for fusion class

--- lib/fission/fusion.rb
+++ lib/fission/fusion.rb
@@ -1,6 +1,14 @@
 module Fission
   class Fusion
 
+    # Public: Determines if the VMware Fusion GUI application is running.
+    #
+    # Examples
+    #
+    #   Fission::Fusion.is_running?
+    #   # => true
+    #
+    # Returns a Boolean.
     def self.is_running?
       command = "ps -ef | grep -v grep | grep -c "
       command << "#{Fission.config['gui_bin'].gsub(' ', '\ ')} 2>&1"
