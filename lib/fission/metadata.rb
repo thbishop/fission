11bb648ba250ade19e83fd43356f974036aed4b5
Tommy Bishop <bishop.thomas@gmail.com>
2011-10-04 16:20:50 -0700

    documentation for metadata class

--- lib/fission/metadata.rb
+++ lib/fission/metadata.rb
@@ -5,6 +5,20 @@ module Fission
 
     attr_accessor :content
 
+    # Public: Deletes the Fusion metadata related to a VM.  The VM should not be
+    # running when this method is called.  It's highly recommended to call this
+    # method without the Fusion GUI application running.  If the Fusion GUI is
+    # running this method should succeed, but it's been observed that Fusion
+    # will recreate the metadata which is deleted.  This leads to 'missing' VMs
+    # in the Fusion GUI.
+    #
+    # vm_path - The absolute path to the directory of a VM.
+    #
+    # Examples
+    #
+    #   Fission::Metadata.delete_vm_info '/vms/foo.vmwarevm'
+    #
+    # Returns nothing.
     def self.delete_vm_info(vm_path)
       metadata = new
       metadata.load
@@ -13,11 +27,27 @@ module Fission
       metadata.save
     end
 
+    # Public: Reads the configured metadata file and populates the content
+    # variable with native ruby types.
+    #
+    # Examples
+    #
+    #   metadata.load
+    #
+    # Returns nothing.
     def load
       raw_data = CFPropertyList::List.new :file => Fission.config['plist_file']
       @content = CFPropertyList.native_types raw_data.value
     end
 
+    # Public: Saves a new version of the metadata file with the data in the
+    # content variable.
+    #
+    # Examples
+    #
+    #   metadata.save
+    #
+    # Returns nothing.
     def save
       new_content = CFPropertyList::List.new
       new_content.value = CFPropertyList.guess @content
@@ -25,12 +55,34 @@ module Fission
                        CFPropertyList::List::FORMAT_BINARY
     end
 
+    # Public: Deletes the VM information from the 'restart document path'
+    # metadata. The 'restart document path' dictates which GUI consoles to
+    # display when Fusion starts.
+    #
+    # vm_path - The absolute path to the directory of a VM.
+    #
+    # Examples
+    #
+    #   metadata.delete_vm_restart_document 'vms/foo.vmwarevm'
+    #
+    # Returns nothing.
     def delete_vm_restart_document(vm_path)
       if @content.has_key?('PLRestartDocumentPaths')
         @content['PLRestartDocumentPaths'].delete_if { |p| p == vm_path }
       end
     end
 
+    # Public: Deletes the VM information from the 'favorites list' metadata.
+    # The 'favorites list' dictates which VMs are displayed in the Fusion VM
+    # libarary.
+    #
+    # vm_path - The absolute path to the directory of a VM.
+    #
+    # Examples
+    #
+    #   metadata.delete_favorite_entry '/vms/foo.vmwarevm'
+    #
+    # Returns nothing.
     def delete_vm_favorite_entry(vm_path)
       @content['VMFavoritesListDefaults2'].delete_if { |vm| vm['path'] == vm_path }
     end
