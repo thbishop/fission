46b9beeddae1080e0c355ca57ae58b1c945f7ec3
Tommy Bishop <bishop.thomas@gmail.com>
2011-10-04 13:44:02 -0700

    documentation for command_helpers module

--- lib/fission/command_helpers.rb
+++ lib/fission/command_helpers.rb
@@ -1,6 +1,16 @@
 module Fission
   module CommandHelpers
 
+    # Public: Checks for the existence of a VM and outputs and error and exits
+    # if it is not found.
+    #
+    # vm_name - The name of the VM to look for.
+    #
+    # Examples
+    #
+    #   ensure_vm_exists 'foo'
+    #
+    # Returns nothing.
     def ensure_vm_exists(vm_name)
       exists_response = VM.exists? vm_name
 
