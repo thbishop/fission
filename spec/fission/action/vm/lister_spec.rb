bf09d1f48d8b29a2df07119f874abcf5bf349209
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-22 01:00:05 -0700

    fix vm lister spec on travis

--- spec/fission/action/vm/lister_spec.rb
+++ spec/fission/action/vm/lister_spec.rb
@@ -111,7 +111,7 @@ describe Fission::Action::VM::Lister do
 
       response = @lister.all_running
       response.should be_a_successful_response
-      response.data.should == [@vm_1, @vm_2, @vm_3]
+      response.data.sort.should == [@vm_1, @vm_2, @vm_3].sort
     end
 
     it 'should return an unsuccessful response if unable to get the list of running vms' do
